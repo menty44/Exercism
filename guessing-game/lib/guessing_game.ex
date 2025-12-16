58b7329b89cfe292385a9c497029a1d3af7f6513
Fredrick Oluoch <fred.oluoch@blaqueyard.com>
2025-12-16 13:57:35 +0300

    Guessing game solved

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,26 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+  def compare(secret_number) do
+    "Make a guess"
+  end
+
+  def compare(secret_number, :no_guess) do
+    "Make a guess"
+  end
+
+  def compare(secret_number, guess) when secret_number == guess do
+    "Correct"
+  end
+
+  def compare(secret_number, guess)
+      when abs(secret_number - guess) == 1 or abs(secret_number + guess) == 1 do
+    "So close"
+  end
+
+  def compare(secret_number, guess) when secret_number > guess do
+    "Too low"
+  end
+
+  def compare(secret_number, guess) when secret_number < guess do
+    "Too high"
   end
 end
